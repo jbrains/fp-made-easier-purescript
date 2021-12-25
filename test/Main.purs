9436e80721ec799a930e62fed17d0d86addc3e41
J. B. Rainsberger <me@jbrains.ca>
2021-12-25 11:17:16 -0400

    Made it easier to build parts of Prelude myself as an exercise.

--- test/Main.purs
+++ test/Main.purs
@@ -1,47 +1,47 @@
 module Test.Main where
 
-import Prelude
-
 import Effect (Effect)
 import Effect.Aff (launchAff_)
+import Prelude (Unit, show, discard)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
 
-flip' :: forall a b c. (a -> b -> c) -> (b -> a -> c)
-flip' f = \x y -> f y x
+flip :: forall a b c. (a -> b -> c) -> (b -> a -> c)
+flip f = \x y -> f y x
 
 checkFlip :: Spec Unit
 checkFlip =
     describe "flip" do
         it "applies the correct argument" do
-            (flip' const' 1 2) `shouldEqual` 2
+            (flip const 1 2) `shouldEqual` 2
 
-const' :: forall a b. a -> (b -> a)
-const' x = \_ -> x
+const :: forall a b. a -> (b -> a)
+const x = \_ -> x
 
 checkConst :: Spec Unit
 checkConst =
     describe "const" do
         it "returns its argument" do
-            const' "::return me::" ignoreMe `shouldEqual` "::return me::"
+            const "::return me::" ignoreMe `shouldEqual` "::return me::"
             where
             ignoreMe = 12
 
-apply' :: forall a b. (a -> b) -> a -> b
-apply' f x = f x
+apply :: forall a b. (a -> b) -> a -> b
+apply f x = f x
 
-infixr 0 apply' as $$$
+infixr 0 apply as $
 
 checkApplyOperator :: Spec Unit
 checkApplyOperator =
     describe "$" do
         it "applies the argument correctly" do
-            (show $$$ (flip' const' 1 2)) `shouldEqual` "2"
+            (show $ flip const 1 2) `shouldEqual` "2"
 
 main :: Effect Unit
 main = do
     launchAff_ $ runSpec [ consoleReporter] do
         checkFlip
         checkConst
+        checkApplyOperator
