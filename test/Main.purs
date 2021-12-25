0febeaeb5648510a6d4db4d5e02d2bbac1110c7d
J. B. Rainsberger <me@jbrains.ca>
2021-12-25 12:41:35 -0400

    singleton

--- test/Main.purs
+++ test/Main.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Aff (launchAff_)
+import Data.List (List(..), (:))
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
@@ -43,7 +44,22 @@ checkPrelude =
        checkApplyOperator
        checkApplyFlippedOperator
 
+singleton :: forall a. a -> List a
+singleton x = x : Nil
+
+checkSingleton :: Spec Unit
+checkSingleton =
+    describe "singleton" do
+        it "matches creating a List the long way" do
+            (singleton 12) `shouldEqual` (12 : Nil)
+
+checkDataList :: Spec Unit
+checkDataList =
+    describe "Data.List" do
+        checkSingleton
+
 main :: Effect Unit
 main = do
     launchAff_ $ runSpec [ consoleReporter] do
         checkPrelude
+        checkDataList
