f53db9a52321b5a04d963948fb7cfb0fe34cfcb6
J. B. Rainsberger <me@jbrains.ca>
2021-12-25 10:54:19 -0400

    Wrote flip.

--- test/Main.purs
+++ test/Main.purs
@@ -3,9 +3,23 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
+import Effect.Aff (launchAff_)
 import Effect.Class.Console (log)
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
+
+flip' :: forall a b c. (a -> b -> c) -> (b -> a -> c)
+flip' f = \x y -> f y x
+
+checkFlip :: Spec Unit
+checkFlip =
+    describe "flip" do
+        it "applies the correct argument" do
+            (flip' const 1 2) `shouldEqual` 2
 
 main :: Effect Unit
 main = do
-  log "🍝"
-  log "You should add some tests."
+    launchAff_ $ runSpec [ consoleReporter] do
+        checkFlip
